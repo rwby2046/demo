08675e99f1f2feae1f882f95fae7e919968c2d72
rwby2046 <31496991+rwby2046@users.noreply.github.com>
2022-05-19 23:30:14 +0800

    Add files via upload

---
+++ WebToolUtil.java
@@ -0,0 +1,51 @@
+package com.suning.snfe.util;
+
+import com.suning.snfe.enums.ServiceExceptionEnum;
+import com.suning.snfe.exception.ServiceException;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * @author: 18049267
+ * @date: 2018/9/4
+ * @description:
+ */
+public class WebToolUtil {
+
+    private WebToolUtil() {
+        throw new IllegalAccessError("Utility class");
+    }
+
+    /**
+     * 获取Linux下的IP地址
+     *
+     * @return IP地址
+     * @throws SocketException
+     */
+    public static String getLinuxLocalIp(){
+        String ip = "";
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                String name = intf.getName();
+                if (!name.contains("docker") && !name.contains("lo")) {
+                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        if (!inetAddress.isLoopbackAddress()) {
+                            String ipaddress = inetAddress.getHostAddress();
+                            if (!ipaddress.contains("::") && !ipaddress.contains("0:0:") && !ipaddress.contains("fe80")) {
+                                ip = ipaddress;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw  new ServiceException(ServiceExceptionEnum.GET_IP_ERROR, e);
+        }
+        return ip;
+    }
+}
